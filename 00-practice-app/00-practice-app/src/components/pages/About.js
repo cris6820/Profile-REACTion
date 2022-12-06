7061a059549cea35319534c448e18b754879b740
Cristina Calderon <calderocristina@gmail.com>
2022-12-06 01:01:35 -0700

    added in code files

---
+++ 00-practice-app/00-practice-app/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+      I am an aspiring 19 year old web developer. At the moment my coding skills are not perfect but i will keep working to perfect them! My career goal is to create inovative applications that is loved and constantly used by users.
+      </p>
+    </div>
+  );
+}
